fc585e0d0036f59ac0ba255d8c30646478139665
Rawnak Chourasia <rnkchr0@gmail.com>
2022-09-24 17:13:26 +0530

    Views files added and connected to Controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,8 @@
 module.exports.profile = function (req, res) {
-  res.end("<h1>User Profile</h1>");
+  //   res.end("<h1>User Profile</h1>");
+  res.render("users", {
+    title: "profile",
+  });
 };
 
 module.exports.profilePic = function (req, res) {
