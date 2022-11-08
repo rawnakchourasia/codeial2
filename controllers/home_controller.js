1b996de7340485f8e14a666f4253eb7f94d47034
Rawnak Chourasia <rnkchr0@gmail.com>
2022-11-08 18:42:12 +0530

    Displaying user of the post in - posts display

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,26 @@
+const Post = require("../models/post");
+
 module.exports.home = function (req, res) {
   //   return res.end("<h1>Express is up for Codeial!</h1>");
   // console.log(req.cookies);
-  res.cookie("user_id", 45);
-  return res.render("home", {
-    title: "home",
-  });
+  // res.cookie("user_id", 45);
+
+  // Post.find({}, function (err, posts) {
+  //   return res.render("home", {
+  //     title: "Codeial | Home",
+  //     posts: posts,
+  //   });
+  // });
+
+  // Populate the user of each post
+  Post.find({})
+    .populate("user")
+    .exec(function (err, posts) {
+      return res.render("home", {
+        title: "Codeial | Home",
+        posts: posts,
+      });
+    });
 };
 
 module.exports.settings = function (req, res) {
