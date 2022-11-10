fa679129c09d929477d54934d400593808df5b79
Rawnak Chourasia <rnkchr0@gmail.com>
2022-11-10 16:16:00 +0530

    deleting individual comment

--- routes/comments.js
+++ routes/comments.js
@@ -5,6 +5,11 @@ const passport = require("passport");
 const commentsController = require("../controllers/comments_controller");
 
 router.post("/create", passport.checkAuthentication, commentsController.create);
+router.get(
+  "/destroy/:id",
+  passport.checkAuthentication,
+  commentsController.destroy
+);
 
 // router.get("/text", commentsController.textPost);
 // router.get("/image", commentsController.imagePost);
