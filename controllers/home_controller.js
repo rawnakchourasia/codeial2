6873692d8bd0493dcbb1d1683c34f925f0560c94
Rawnak Chourasia <rnkchr0@gmail.com>
2022-11-24 17:07:00 +0530

    async await | try catch

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,36 +1,28 @@
 const Post = require("../models/post");
 const User = require("../models/user");
 
-module.exports.home = function (req, res) {
-  //   return res.end("<h1>Express is up for Codeial!</h1>");
-  // console.log(req.cookies);
-  // res.cookie("user_id", 45);
-
-  // Post.find({}, function (err, posts) {
-  //   return res.render("home", {
-  //     title: "Codeial | Home",
-  //     posts: posts,
-  //   });
-  // });
-
-  // Populate the user of each post
-  Post.find({})
-    .populate("user")
-    .populate({
-      path: "comments",
-      populate: {
-        path: "user",
-      },
-    })
-    .exec(function (err, posts) {
-      User.find({}, function (err, users) {
-        return res.render("home", {
-          title: "Codeial | Home",
-          posts: posts,
-          all_users: users,
-        });
+module.exports.home = async function (req, res) {
+  try {
+    // Populate the user of each post
+    let posts = await Post.find({})
+      .populate("user")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+        },
       });
+
+    let users = await User.find({});
+    return res.render("home", {
+      title: "Codeial | Home",
+      posts: posts,
+      all_users: users,
     });
+  } catch (error) {
+    console.log("Error", error);
+    return;
+  }
 };
 
 module.exports.settings = function (req, res) {
