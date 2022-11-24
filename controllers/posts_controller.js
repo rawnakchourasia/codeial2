8d20e740c75debec6ed8c78edfc43594dc5b0b57
Rawnak Chourasia <rnkchr0@gmail.com>
2022-11-24 18:19:29 +0530

    async await in other controllers except users

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,43 +1,36 @@
 const Post = require("../models/post");
 const Comment = require("../models/comment");
 
-module.exports.create = function (req, res) {
-  Post.create(
-    {
+module.exports.create = async function (req, res) {
+  try {
+    await Post.create({
       content: req.body.content,
       user: req.user._id,
-    },
-    function (err, post) {
-      if (err) {
-        console.log("Error in creating a Post");
-        return;
-      }
-      return res.redirect("back");
-    }
-  );
+    });
+    return res.redirect("back");
+  } catch (error) {
+    console.log("Error", error);
+    return;
+  }
 };
 
-module.exports.destroy = function (req, res) {
-  Post.findById(req.params.id, function (err, post) {
+module.exports.destroy = async function (req, res) {
+  try {
+    let post = await Post.findById(req.params.id);
     // .id means converting the object id into string
     console.log("1");
     if (String(post.user).valueOf() === String(req.user.id).valueOf()) {
       post.remove();
       console.log("2");
 
-      Comment.deleteMany({ post: req.params.id }, function (err) {
+      await Comment.deleteMany({ post: req.params.id }, function (err) {
         return res.redirect("back");
       });
     } else {
       return res.redirect("back");
     }
-  });
+  } catch (error) {
+    console.log("Error", error);
+    return;
+  }
 };
-
-// module.exports.textPost = function (req, res) {
-//   res.end("<h1>Text Post</h1>");
-// };
-
-// module.exports.imagePost = function (req, res) {
-//   res.end("<h1>Image Post</h1>");
-// };
